1ff2b821eccfecd8378d6fbbd9d04aac42ce823e
Sahil Johari <darewithsj@gmail.com>
2022-01-29 11:30:25 -0500

    Add firebase files

---
+++ configs/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_STORAGE_BUCKET,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_APP_ID,
+  messagingSenderId: "123949569144",
+  measurementId: "G-785YF4G76C",
+};
+
+export default config;
